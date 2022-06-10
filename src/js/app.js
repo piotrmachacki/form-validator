c5ec8c5307386fbc949df6fa4475c3d4fec6e575
Piotr Machacki <piotr@machacki.pl>
2022-06-10 10:17:16 +0200

    Project configuration

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import Generator from './generator';
+import Validator from './validator';
+
+console.log('GENERATOR', Generator);
+console.log('VALIDATOR', Validator);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'none',
+    target: 'web',
+    entry: [
+        '/src/js/app.js',
+        '/src/sass/app.scss',
+    ],
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'dist/'),
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            inject: true,
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessorPluginOptions: {
+                preset: ['default', { discardComments: { removeAll: true } }],
+            },
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { outputPath: 'css/', name: '[name].min.css' },
+                    },
+                    'extract-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+};
